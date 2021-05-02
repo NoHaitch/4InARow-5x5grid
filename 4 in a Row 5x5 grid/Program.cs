c34a4a66fc6ce56e40bcd1ba4622a7a7bf563be2
Killershark77 <franciscotrianto@gmail.com>
2021-05-02 14:19:30 +0700

    Add project files.

---
+++ 4 in a Row 5x5 grid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_in_a_Row_5x5_grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
